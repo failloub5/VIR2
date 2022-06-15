3d3fe964b8881a0216118c20d91af388c4a3c4dc
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-15 23:22:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ git clone https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -nc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
 docker-compose up -d
-docker-compose exec websrv docker-php-ext-install mysql
-docker-compose exec websrv /etc/init.d/apache2 reload
+docker-compose exec -T websrv docker-php-ext-install mysql
+docker-compose exec -T websrv /etc/init.d/apache2 reload
