b1a37e4fe915b67c6d81e452f2fa0e50f793269e
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-15 22:43:32 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,30 +1,9 @@
-#!/bin/bash
-DOCKER_COMPOSE="version: '3'
-services:
-  websrv:
-    image: php:5-apache
-    ports:
-      - "80:80"
-      - "443:443"
-    volumes:
-      - ~/Photoblog:/var/www/html
-  db:
-    image: mysql:5
-    ports:
-      - "3306:3306"
-    environment:
-      - MYSQL_ROOT_PASSWORD=password
-      - MYSQL_USER=pentesterlab
-      - MYSQL_PASSWORD=pentesterlab
-      - MYSQL_DATABASE=photoblog
-    volumes:
-      - ~/Photoblog/photoblog.sql:/docker-entrypoint-initdb.d/photoblog.sql"
-
+#!/bin/sh
 apt update -y
-apt install -y docker docker-compose git
+apt install -y docker docker-compose git wget
 git clone https://github.com/7ric/Photoblog.git
 sed -i 's/localhost/db/g' ~/Photoblog/classes/db.php
-echo $DOCKER_COMPOSE > docker-compose.yaml
+wget https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
 docker-compose up -d
 docker-compose exec websrv docker-php-ext-install mysql
 docker-compose exec websrv /etc/init.d/apache2 reload
