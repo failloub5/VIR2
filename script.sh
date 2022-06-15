5f65bd7a5a1246b097865794c6c048ba629d0e84
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-15 23:16:36 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 apt update -y
 apt install -y docker docker-compose git wget
-git clone https://github.com/7ric/Photoblog.git
-sed -i 's/localhost/db/g' ~/Photoblog/classes/db.php
+git clone https://github.com/7ric/Photoblog.git /Photoblog
+sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -nc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
 docker-compose up -d
 docker-compose exec websrv docker-php-ext-install mysql
