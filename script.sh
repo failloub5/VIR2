2172e46ca9dc4a1939863b75a81d2a32d7a8c1ba
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-22 23:40:38 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,8 @@ echo "Pret a devenir un hackeres ????" | /usr/games/lolcat
 git clone -q https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -nc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
-docker-compose up -d --quiet-pull
+docker-compose up -d --quiet-pull websrv db
 echo "le super" | /usr/games/lolcat
 docker-compose exec -T websrv docker-php-ext-install mysql &>/dev/null
 docker-compose exec -T websrv /etc/init.d/apache2 reload &>/dev/null
-exec docker run --tty --interactive kalilinux/kali-rolling /bin/bash -c 'apt update &>/dev/null; apt install -y sqlmap lynx w3m links &>/dev/null; exec bash'
-
+exec docker-compose run kali
