0efd89b78f0eb16e825e2e5bcdd787fa64ffe6d1
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-22 23:58:50 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ git clone -q https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -qnc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
 docker-compose pull -q 
-docker-compose up -d -q websrv db
+docker-compose up -d websrv db
 docker-compose exec -T websrv docker-php-ext-install mysql &>/dev/null
 docker-compose exec -T websrv /etc/init.d/apache2 reload &>/dev/null
 echo "site web accessible sur http://websrv" | /usr/games/lolcat
