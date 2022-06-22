fff93fdd30768fc13a5de1f0f2e56b9d759e1846
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-22 23:51:56 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,8 @@ echo "Preparation du Pentestlab..." | /usr/games/lolcat
 git clone -q https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -qnc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
-docker-compose up -d --quiet websrv db
+docker-compose pull -q 
+docker-compose up -d -q websrv db
 docker-compose exec -T websrv docker-php-ext-install mysql &>/dev/null
 docker-compose exec -T websrv /etc/init.d/apache2 reload &>/dev/null
 exec docker-compose run --quiet-pull kali
