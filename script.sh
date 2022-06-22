3df030cd33605bd6253f17555c555dd3f914105a
failloub5 <56110193+failloub5@users.noreply.github.com>
2022-06-22 22:19:44 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ git clone https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -nc https://raw.githubusercontent.com/failloub5/VIR2/main/docker-compose.yaml
 docker-compose up -d
-docker-compose exec -T websrv docker-php-ext-install mysql
-docker-compose exec -T websrv /etc/init.d/apache2 reload
+docker-compose exec websrv docker-php-ext-install mysql
+docker-compose exec websrv /etc/init.d/apache2 reload
 exec docker run --tty --interactive kalilinux/kali-rolling /bin/bash -c 'apt update; apt install -y sqlmap lynx w3m links; exec bash'
 # docker build -t dockerfile .
 # docker run -it dockerfile bash
